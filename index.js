a94e6aac4cc2ab5ed2a2dc19093a87e0c12e36f0
devkiratu <dev.kiratu@gmail.com>
2020-11-06 20:29:49 +0300

    version 0.1 - +modal +full functionality, polish styling

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const input = document.querySelector("textarea");
 const notesBoard = document.querySelector(".notes-board");
 const noNotes = document.querySelector("#no-notes");
 const addBtn = document.querySelector("#add");
+const modal = document.querySelector(".modal");
+const modalCloseBtn = document.querySelector("#close");
+const modalContent = document.querySelector("#content");
 window.addEventListener("load", input.focus());
 
 addBtn.addEventListener("click", (e) => {
@@ -11,11 +14,33 @@ addBtn.addEventListener("click", (e) => {
 	} else {
 		console.log("hi");
 		noNotes.hidden = true;
+		let noteWrapper = document.createElement("div");
+		noteWrapper.setAttribute("id", "wrapper");
+		let details = document.createElement("button");
+		details.setAttribute("id", "view-details");
+		details.innerHTML = "View details";
 		let sticky = document.createElement("p");
 		sticky.setAttribute("id", "new-note");
-		sticky.innerHTML = input.value;
-		notesBoard.appendChild(sticky);
+		sticky.innerHTML = `${input.value.substring(0, 30)}...`;
+		noteWrapper.appendChild(sticky);
+		noteWrapper.appendChild(details);
+		notesBoard.appendChild(noteWrapper);
+		let modalSticky = input.value;
 		input.value = "";
 		input.focus();
+		details.addEventListener("click", () => {
+			// console.log(modalContent.lastChild);
+			modalContent.lastChild.remove();
+			let modalP = document.createElement("p");
+			modalP.setAttribute("id", "modal-para");
+			modalP.innerHTML = modalSticky;
+			modalContent.appendChild(modalP);
+			modal.classList.toggle("show");
+		});
 	}
 });
+
+modalCloseBtn.addEventListener("click", () => modal.classList.toggle("show"));
+window.addEventListener("click", (e) =>
+	e.target == modal ? modal.classList.toggle("show") : null
+);
